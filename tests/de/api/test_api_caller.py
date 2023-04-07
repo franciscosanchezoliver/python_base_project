e9b558e2ea8d2b89bf73b7726f11ef0ef495cfab
spuzi <spuzi@hotmail.es>
2023-04-07 10:36:55 +0200

    Delete tests/de directory

--- tests/de/api/test_api_caller.py
+++
@@ -1,8 +0,0 @@
-from src.de.api.api_caller import ApiCaller
-
-
-def test_constructor():
-    api_token = "test"
-    apicaller = ApiCaller(url="http://api.marketstack.com/v1/eod?",
-                          ticker="AAPL",
-                          access_key=api_token)
